1108176ec579aef85acaa0cf2e8448122c3b69be
d22cs051 <d22cs051@iitj.ac.in>
2022-09-19 22:53:15 +0530

    image resized and h_tuning done

--- plot_digits_classification.py
+++ plot_digits_classification.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 # Import datasets, classifiers and performance metrics
 from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
+from skimage.transform import resize
+import numpy as np
 
 # Range of parms
 gamma_list = [0.1,0.005,0.001,0.0005,0.0001]
@@ -36,73 +38,84 @@ for ax, image, label in zip(axes, digits.images, digits.target):
 # PART: data pre-processing -- to normalized data, format the data to consumed by mode
 # flatten the images
 n_samples = len(digits.images)
-data = digits.images.reshape((n_samples, -1))
+resized_image_dataset = [10,16,32]
+for size in resized_image_dataset:
+    new_data_set = []
+    for image in digits.images:
+        image_resized = resize(image, (size,size),anti_aliasing=True)
+        new_data_set.append(image_resized)
+    new_data_set = np.array(new_data_set)
+    # print(image_resized)
+    data = new_data_set.reshape((n_samples, -1))
+    # exit()
+
+    # variables to get best accuracy and best model
+    best_acc = -1.0
+    best_model = None
+    best_h_parms = None
+    for cur_h_parms in h_parms_list:
+        # PART: Define the model
+        # Create a classifier: a support vector classifier
+        clf = svm.SVC()
+        # PART: setting up hyper parameter
+        # hyper_parms = {'gamma': i['gamma'],'C':i['c']}
+        hyper_parms = cur_h_parms
+        clf.set_params(**hyper_parms)
+
+        # PART: define  train/dev/test data splits
+        # Split data into 50% train and 50% test subsets
+        X_train, X_dev_test, y_train, y_dev_test = train_test_split(
+            data, digits.target, test_size=1-train_frac, shuffle=True
+        )
 
 
-# variables to get best accuracy and best model
-best_acc = -1.0
-best_model = None
-best_h_parms = None
-for cur_h_parms in h_parms_list:
-    # PART: Define the model
-    # Create a classifier: a support vector classifier
-    clf = svm.SVC()
-    # PART: setting up hyper parameter
-    # hyper_parms = {'gamma': i['gamma'],'C':i['c']}
-    hyper_parms = cur_h_parms
-    clf.set_params(**hyper_parms)
+        X_test, X_dev, y_test, y_dev = train_test_split(
+            X_dev_test, y_dev_test, test_size=(dev_frac)/(test_frac+dev_frac), shuffle=True
+        )
+        # Train to train model
+        # Dev to tune hyperparameters
+        # test to evalute the performance of the model
 
-    # PART: define  train/dev/test data splits
-    # Split data into 50% train and 50% test subsets
-    X_train, X_dev_test, y_train, y_dev_test = train_test_split(
-        data, digits.target, test_size=1-train_frac, shuffle=True
-    )
+        # Test on unseen data
 
 
-    X_test, X_dev, y_test, y_dev = train_test_split(
-        X_dev_test, y_dev_test, test_size=(dev_frac)/(test_frac+dev_frac), shuffle=True
-    )
-    # Train to train model
-    # Dev to tune hyperparameters
-    # test to evalute the performance of the model
+        # Learn the digits on the train subset
+        clf.fit(X_train, y_train)
 
-    # Test on unseen data
-
-
-    # Learn the digits on the train subset
-    clf.fit(X_train, y_train)
+        # Predict the value of the digit on the test subset
+        predicted_dev = clf.predict(X_dev)
+        
+        cur_accuracy = metrics.accuracy_score(y_pred=predicted_dev,y_true=y_dev)
+        
+        
+        if cur_accuracy > best_acc:
+            best_acc = cur_accuracy
+            best_model = clf
+            best_h_parms = cur_h_parms
+            print(f"parms:- {cur_h_parms}, accuracy:- {cur_accuracy}")
 
     # Predict the value of the digit on the test subset
-    predicted_dev = clf.predict(X_dev)
-    
-    cur_accuracy = metrics.accuracy_score(y_pred=predicted_dev,y_true=y_dev)
-    
-    if cur_accuracy > best_acc:
-        best_acc = cur_accuracy
-        best_model = clf
-        best_h_parms = cur_h_parms
+    predicted = best_model.predict(X_test)
 
-# Predict the value of the digit on the test subset
-predicted = best_model.predict(X_test)
+    # PART: Sanity check for predection
+    # _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
+    # for ax, image, prediction in zip(axes, X_test, predicted):
+    #     ax.set_axis_off()
+    #     image = image.reshape(8, 8)
+    #     ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
+    #     ax.set_title(f"Prediction: {prediction}")
 
-# PART: Sanity check for predection
-_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, prediction in zip(axes, X_test, predicted):
-    ax.set_axis_off()
-    image = image.reshape(8, 8)
-    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-    ax.set_title(f"Prediction: {prediction}")
+    # PART: evulation Matrix computations
+    print("Results")
+    print(f"best parms:- {best_h_parms},best accuracy:- {best_acc}")
 
-# PART: evulation Matrix computations
-print(cur_h_parms)
-print(
-    f"Classification report for classifier {clf}:\n"
-    f"{metrics.classification_report(y_test, predicted)}\n"
-)
+    print(
+        f"Classification report for classifier {best_model}:\n"
+        f"{metrics.classification_report(y_test, predicted)}\n"
+    )
 
+    # disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
+    # disp.figure_.suptitle("Confusion Matrix")
+    # print(f"Confusion matrix:\n{disp.confusion_matrix}")
 
-# disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
-# disp.figure_.suptitle("Confusion Matrix")
-# print(f"Confusion matrix:\n{disp.confusion_matrix}")
-
-# plt.show()
+    # plt.show()
